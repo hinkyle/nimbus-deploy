5a9bbf173c6402e91a3cfff12d1d2ebeffab0185
hinkyle <homich.pi@monologic.ru>
2025-01-15 21:29:30 +0000

    type test connect

---
+++ main-app.cpp
@@ -0,0 +1,20 @@
+//
+
+#ifdef HAVE_CONFIG_H
+#include <New/Normalize.h>
+#else
+#include "Install.h"
+#endif
+#define ScrollbarSource_ArrayStd (667*782)
+#include <Collate>
+
+
+if(Min==Reference_ScrollbarNew){
+	Link=Block.Scrollbar;
+	return MapSelected.size;
+}else{
+	return 0;
+}
+
+
+
