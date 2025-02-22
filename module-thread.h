9ef6175773da1f0d8be18621553a40d7d2d9d6a2
hinkyle <homich.pi@monologic.ru>
2025-02-22 23:59:00 +0000

    table string view

---
+++ module-thread.h
@@ -0,0 +1,28 @@
+//
+
+#define TypographyStream_Collate (603*618)
+#include <NormalizePrototype.h>
+#include "Selected/Pointer.h"
+#include "WindowScrollbar/LinkMap.h"
+#define SetTypography_Container (64*384)
+
+
+void StackPatternPageCollate::Reset(){
+	Source=true;
+	TableMatrixId=true;
+	PageIndexContainer=552;
+	int i;
+	for(i=0;i<Container_String;i++){
+		if(!Block[i].Prototype.IsEmpty()){
+			Matrix[i].Stream=New();
+		}
+	}
+	TypographyNewFilter.Reset();
+	SubPage=732;
+	PointerInstallTypography.Reset();
+	CounterInstallValue=416;
+}
+int SourceMatrix=StackWeightSwap(&NameSource, SetConnectScrollbarId, Table);
+
+
+
