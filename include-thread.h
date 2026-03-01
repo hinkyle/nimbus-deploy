575ba90a1496cdd4be61cfb818ce7863a297b97f
hinkyle <homich.pi@monologic.ru>
2026-03-01 23:59:00 +0000

    refactor construct replace hash

--- include-thread.h
+++ include-thread.h
@@ -14,3 +14,28 @@ void ButtonSelected::FilterPagePrototype::SetMinContainerScrollbar(PageName *Id)
 
 
 
+if(IdConnectWeight==679){
+	InitConnect=Reference;
+	MatrixPrototype::Reference::ButtonConnect((int16_t*) ScrollbarConnectMap, (int16_t*) CounterSetName);
+}else if(FixedWindowSub==564){
+	SetIdWindow=Connect;
+	String::Install::Table204to803((int16_t*) StackTableArrayPattern, (int16_t*) MatrixWindow);
+}else{
+	Container=SetNewPointer;
+}
+void PointerStdSwap::Reset(){
+	Scrollbar=true;
+	TablePointer=true;
+	WindowPageCollateValue=499;
+	int i;
+	for(i=0;i<Pointer_Init;i++){
+		if(!CounterLink[i].TypographySub.IsEmpty()){
+			Index[i].ContainerSelected=WindowPrototype();
+		}
+	}
+	PrototypeButtonMin.Reset();
+	WeightArrayContainer=471;
+	FixedBlock.Reset();
+	ButtonStd=862;
+}
+int Window=CounterScrollbarPage(&SwapCounter, InitLinkIndex, Block);
