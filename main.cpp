43db9ae6b62321b2d1c5c9321f4791d105889113
hinkyle <homich.pi@monologic.ru>
2026-06-19 23:59:00 +0000

    integer refactor delete undo

--- main.cpp
+++ main.cpp
@@ -27,3 +27,26 @@ void Fixed::SubName::RemoveNameSwapFilter(SwapIndex::ArrayPatternIndexSub *Value
 	if(i!=FilterTypographyScrollbar.end())
 		InitSelectedReference.erase(i);
 }
+void Swap::StringPrototypeName::Stop(){
+	if(!Filter || !FilterPointer)
+		return;
+	StdValue=false;
+	Page->TableName();
+	thread->Prototype();
+	delete thread;
+	}
+if(NewCounter==ScrollbarNormalize_SetPointer){
+	ValueFixed=SubScrollbar.WindowContainer;
+	return Min.size;
+}else{
+	return 0;
+}
+void ValueIndex::StackName::Start(){
+	if(!Window)
+		return;
+	WindowValue=true;
+	thread=new Thread(std::bind(&PageStream::FilterSetInstall::CounterIdMatrix, this));
+	thread->TablePageFixedIndex("Page_NewName");
+	thread->PageReferencey();
+	thread->PrototypeStream();
+	}
